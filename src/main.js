a8ffce2d9cadb33dee54f6cb52b479f3a388b848
MAGIC <tianzhonghiroki82@gmail.com>
2025-10-11 07:10:34 +0900

    update

--- src/main.js
+++ src/main.js
@@ -276,7 +276,7 @@ function attachCellEvents(){
       body.innerHTML = `
         <h3>授業内容</h3>
         <p>ここに授業の詳細情報を表示します。</p>
-        <p>セル位置: 行 ${rowIndex + 1}, 列 ${colIndex + 1}</p>
+        <p>セル位置: 行 ${row + 1}, 列 ${col + 1}</p>
         <p>${(row-2)%6 + Math.floor(col/6)}</p>
       `;
 
