4ecbf3c18dff3269cec4b7c1c2cb74991c286603
MAGIC <tianzhonghiroki82@gmail.com>
2025-10-15 05:21:55 +0900

    update

--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ async function firstLiff(){
     const profile = await liff.getProfile();
     const userIdElem = document.getElementById("userId");
     userIdElem.textContent = profile.userId;
-    userIdElem.style.fontSize = "13px";
+    userIdElem.style.fontSize = "12px";
 
     return profile.userId;
   }catch (error){
